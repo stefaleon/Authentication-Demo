773b126475dfd9a5c5e7ba42a42b86589a363a90
maimous@gmail.com <stefaleon@gmail.com>
2016-11-17 11:11:12 +0200

    added isLoggedIn middleware to the secret route

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/', function(req, res) {
 });
 
 
-app.get('/secret', function(req, res) {
+app.get('/secret', isLoggedIn, function(req, res) {
 	res.render('secret');
 });
 
@@ -76,6 +76,13 @@ app.get('/logout', function(req, res) {
 	res.redirect('/');
 });
 
+
+function isLoggedIn(req, res, next){
+	if (req.isAuthenticated()){
+		return next();
+	}
+	res.redirect('/login');
+}
 
 
 
